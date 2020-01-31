cb62ca3ed5eb868e0479ffeb16fd46d840ef15d4
Ricardo-Paul <ricardopaul459@gmail.com>
2020-01-31 17:20:57 -0500

    add name field

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::API
+    protect_from_forgery with: :null_session
+
+    before_filter :configure_permitted_parameters, if: :devise_controller?
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+    end
+end
