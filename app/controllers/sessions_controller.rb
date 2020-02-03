68406608d40a65d669637080c433e1956de47826
Ricardo-Paul <ricardopaul459@gmail.com>
2020-02-03 10:15:25 -0500

    create destry sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+    def create
+        user_email = params[:sessions][:email]
+        user_password = params[:sessions][:password]
+
+        user = user_email.present? && User.find_by(email: user_email)
+        if user.valid_password? user_password
+            sign_in user, store: false
+            user.generate_authentication_token!
+            user.save
+            render json: user, status: 200, message: "logged in"
+        else
+            render json: {errors: "Invalid email or password"}, status: 422
+        end
+    end
+
+    def destroy
+        user = User.find_by(auth_token: request.headers["Authorization"])
+        user.save
+        head 204
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   scope '/api/v1' do
     resources :users, :only => [:index, :create, :show, :update, :destroy]
+    resource :sessions, :only => [:create, :destroy]
   end
 end
